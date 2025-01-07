848abd797a67c364091a776782648e371c3e5bd6
GrivianDev <grivian-dev@posteo.de>
2025-01-07 22:26:04 +0100

    Class for mappinh blocktype to texture indices in textureatlas

---
+++ src/core/engine/rendering/BlockToTextureMapping.h
@@ -0,0 +1,20 @@
+#ifndef BLOCKTOTEXTUREMAPPING_H
+#define BLOCKTOTEXTUREMAPPING_H
+
+#include "datatypes/DatatypeDefs.h"
+#include "datatypes/BlockTypes.h"
+#include <cstdint>
+#include <array>
+#include <unordered_map>
+
+class BlockToTextureMap {
+private:
+    std::unordered_map<uint16_t, std::array<uint16_t, 6>> map;
+    
+public:
+    BlockToTextureMap();
+
+    const uint16_t getTexIndex(uint16_t blockType, AxisDirection dir) const;
+};
+
+#endif
