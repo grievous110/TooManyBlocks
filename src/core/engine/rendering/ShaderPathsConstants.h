cef3bb7f1004dbdcd948936243676708b7ff017f
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-01-20 00:06:30 +0100

    Update ShaderPathsConstants.h

--- src/core/engine/rendering/ShaderPathsConstants.h
+++ src/core/engine/rendering/ShaderPathsConstants.h
@@ -2,6 +2,7 @@
 #define SHADERPATHCONSTANTS_H
 
 #define DEPTH_SHADER "res/shaders/depthShader"
+#define CHUNK_DEPTH_SHADER "res/shaders/chunkDepthShader"
 #define SIMPLE_SHADER "res/shaders/simpleShader"
 #define CHUNK_SHADER "res/shaders/chunkShader"
 
