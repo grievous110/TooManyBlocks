d9756cffa7553bba656636fd617c71f480d1d08f
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-02-06 22:23:35 +0100

    Update FrameBuffer.h

--- src/core/engine/rendering/lowlevelapi/FrameBuffer.h
+++ src/core/engine/rendering/lowlevelapi/FrameBuffer.h
@@ -1,26 +1,36 @@
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
 
-#include "engine/rendering/lowlevelapi/Texture.h"
 #include "engine/rendering/lowlevelapi/RenderApiObject.h"
+#include "engine/rendering/lowlevelapi/Texture.h"
+#include <memory>
+#include <unordered_map>
 
-// Currently only used for DepthTextures
 class FrameBuffer : public RenderApiObject {
 private:
     static thread_local unsigned int currentlyBoundFBO;
-    Texture* m_depthTexture;
+    std::unordered_map<unsigned int, std::shared_ptr<Texture>> m_attachedTextures;
+    std::shared_ptr<Texture> m_attachedDepthTexture;
+
+    void finalizeDrawBufferOutput();
 
 public:
     static void bindDefault();
 	static void syncBinding();
 
-    FrameBuffer(unsigned int width, unsigned int height);
+    FrameBuffer();
     FrameBuffer(FrameBuffer&& other) noexcept;
     virtual ~FrameBuffer();
 
     void bind() const;
     
-    Texture* getDepthTexture() const { return m_depthTexture; }
+    void attachTexture(std::shared_ptr<Texture> texture, unsigned int attachmentPoint = 0);
+
+    void clearAttachedTextures();
+
+    inline const std::unordered_map<unsigned int, std::shared_ptr<Texture>>& getAttachedTextures() const { return m_attachedTextures; }
+
+    inline std::shared_ptr<Texture> getAttachedDepthTexture() const { return m_attachedDepthTexture; }
 
     FrameBuffer& operator=(FrameBuffer&& other) noexcept;
 };
