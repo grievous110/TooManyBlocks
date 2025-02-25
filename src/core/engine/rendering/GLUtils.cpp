2f4a5b4df5b27dc70592fafa98e211432ad89bb5
GrivianDev <grivian-dev@posteo.de>
2025-02-25 12:46:21 +0100

    GL helper improvements

--- src/core/engine/rendering/GLUtils.cpp
+++ src/core/engine/rendering/GLUtils.cpp
@@ -2,15 +2,13 @@
 #include "Logger.h"
 #include <gl/glew.h>
 #include <sstream>
-#include <vector>
+
+static constexpr int maxErrorChecks = 10;
 
 void GLClearError() {
-	const int maxChecks = 10;  // Limit number of error checks
     int errorCount = 0;
-
     while (glGetError() != GL_NO_ERROR) {
-        errorCount++;
-        if (errorCount >= maxChecks) {
+        if (++errorCount >= maxErrorChecks) {
             lgr::lout.error("GLClearError() reached maximum error checks. Possible infinite error generation.");
             break;
         }
@@ -18,27 +16,20 @@ void GLClearError() {
 }
 
 bool GLLogCall(const char* functionName, const char* file, int line) {
-	const int maxChecks = 10;  // Limit number of error checks
     int errorCount = 0;
-	std::vector<GLenum> errors;
-	while (GLenum error = glGetError()) {
-		errors.push_back(error);
-		errorCount++;
-		if (errorCount >= maxChecks) {
-            lgr::lout.error("GLClearError() reached maximum error checks. Possible infinite error generation.");
+	GLenum error;
+	while ((error = glGetError()) != GL_NO_ERROR) {
+		std::stringstream stream;
+		stream << "[OpenGL Error 0x" << std::hex << error <<
+			"] caused by " << functionName <<
+			" in " << file <<
+			" line " << line;
+		lgr::lout.error(stream.str());
+
+		if (++errorCount >= maxErrorChecks) {
+			lgr::lout.error("GLLogCall() reached maximum error checks. Possible infinite error generation.");
             break;
         }
 	}
-	if (!errors.empty()) {
-		for (const GLenum& e : errors) {
-			std::stringstream stream;
-			stream << "[OpenGL Error 0x" << std::hex << e <<
-				"] caused by " << functionName <<
-				" in " << file <<
-				" line " << line;
-			lgr::lout.error(stream.str());
-		}
-		return false;
-	}
-	return true;
+	return errorCount == 0;
 }
