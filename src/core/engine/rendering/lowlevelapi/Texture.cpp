be4573067b54a98f5caaf41aeafb2d66de8c4b39
GrivianDev <grivian-dev@posteo.de>
2025-02-26 16:28:54 +0100

    More Texture api control

--- src/core/engine/rendering/lowlevelapi/Texture.cpp
+++ src/core/engine/rendering/lowlevelapi/Texture.cpp
@@ -10,7 +10,7 @@ struct TextureFormat {
     GLenum type;
 };
 
-static TextureFormat getTextureFormat(TextureType type, int channels) {
+static constexpr TextureFormat toOpenGLTexFormat(TextureType type, int channels) {
     switch (type) {
         case TextureType::Color:
             switch (channels) {
@@ -40,11 +40,57 @@ static TextureFormat getTextureFormat(TextureType type, int channels) {
     }
 }
 
+static constexpr GLenum toOpenGLFilterMode(TextureFilter filterMode) {
+	if (filterMode == TextureFilter::Linear) {
+		return GL_LINEAR;
+	}
+	return GL_NEAREST;	
+}
+
+static constexpr GLenum toOpenGLMinFilterModeWithMipmap(TextureFilter baseFilterMode, TextureFilter mipmapFilterMode) {
+	 // Base filter + mipmap filter config param (Only used for GL_TEXTURE_MIN_FILTER parameter meaning minification)
+	if (baseFilterMode == TextureFilter::Linear) {
+        if (mipmapFilterMode == TextureFilter::Linear) {
+            return GL_LINEAR_MIPMAP_LINEAR;
+        } else if (mipmapFilterMode == TextureFilter::Nearest) {
+            return GL_LINEAR_MIPMAP_NEAREST;
+        }
+    } else if (baseFilterMode == TextureFilter::Nearest) {
+        if (mipmapFilterMode == TextureFilter::Linear) {
+            return GL_NEAREST_MIPMAP_LINEAR;
+        } else if (mipmapFilterMode == TextureFilter::Nearest) {
+            return GL_NEAREST_MIPMAP_NEAREST;
+        }
+    }
+
+    // Should never reach here
+    return GL_LINEAR_MIPMAP_LINEAR;
+}
+
+static constexpr GLenum toOpenGLWrapMode(TextureWrap wrapMode) {
+	switch (wrapMode) {
+		case TextureWrap::Repeat: return GL_REPEAT;
+		case TextureWrap::MirroredRepeat: return GL_MIRRORED_REPEAT;
+		case TextureWrap::ClampToEdge: return GL_CLAMP_TO_EDGE;
+		case TextureWrap::ClampToBorder: return GL_CLAMP_TO_BORDER;
+		default: return GL_REPEAT;
+	}
+}
+
 void Texture::bindDefault() {
 	GLCALL(glBindTexture(GL_TEXTURE_2D, 0));
 }
 
-Texture::Texture(const std::string& path) : m_type(TextureType::Color), m_width(0), m_height(0), m_channels(0) {
+Texture::Texture(const std::string& path) :
+	m_type(TextureType::Color),
+	m_width(0),
+	m_height(0),
+	m_channels(4),
+	m_baseFilter(TextureFilter::Nearest),
+	m_mipmapFilter(TextureFilter::Nearest),
+	m_wrapMode(TextureWrap::Repeat),
+	m_hasMipmaps(false) {
+		
     stbi_set_flip_vertically_on_load(1);
 	int width;
 	int height;
@@ -56,12 +102,13 @@ Texture::Texture(const std::string& path) : m_type(TextureType::Color), m_width(
 	GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
 
 	// Configure texture
-	TextureFormat format = getTextureFormat(m_type, m_channels);
+	TextureFormat format = toOpenGLTexFormat(m_type, m_channels);
 	GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, format.internalFormat, m_width, m_height, 0, format.inputFormat, format.type, buffer));
+	// Default filter and wrap mode
 	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
 	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
 
 	if (buffer) {
 		stbi_image_free(buffer);
@@ -70,23 +117,47 @@ Texture::Texture(const std::string& path) : m_type(TextureType::Color), m_width(
 	}
 }
 
-Texture::Texture(TextureType type, unsigned int width, unsigned int height, int channels, const void* data) : m_type(type), m_width(width), m_height(height), m_channels(channels) {
+Texture::Texture(TextureType type, unsigned int width, unsigned int height, int channels, const void* data, TextureFilter filterMode, TextureWrap wrapMode) :
+	m_type(type),
+	m_width(width),
+	m_height(height),
+	m_channels(channels),
+	m_baseFilter(filterMode),
+	m_mipmapFilter(TextureFilter::Nearest),
+	m_wrapMode(wrapMode),
+	m_hasMipmaps(false) {
+
 	GLCALL(glGenTextures(1, &m_rendererId));
 	GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
 	
 	// Configure texture
-	TextureFormat format = getTextureFormat(type, channels);
+	TextureFormat format = toOpenGLTexFormat(type, channels);
 	GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, format.internalFormat, m_width, m_height, 0, format.inputFormat, format.type, data));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+
+	// Filter and wrap mode
+	GLenum filterParam = toOpenGLFilterMode(filterMode);
+	GLenum wrapParam = toOpenGLWrapMode(wrapMode);
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filterParam));
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filterParam)); // Note: Depth textures ignore mag filter
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapParam));
+	GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapParam));
 }
 
-Texture::Texture(Texture&& other) noexcept : RenderApiObject(std::move(other)), m_type(other.m_type), m_width(other.m_width), m_height(other.m_height), m_channels(other.m_channels) {
+Texture::Texture(Texture&& other) noexcept :
+	RenderApiObject(std::move(other)),
+	m_type(other.m_type),
+	m_width(other.m_width),
+	m_height(other.m_height),
+	m_channels(other.m_channels),
+	m_baseFilter(other.m_baseFilter),
+	m_mipmapFilter(other.m_mipmapFilter),
+	m_wrapMode(other.m_wrapMode),
+	m_hasMipmaps(other.m_hasMipmaps) {
+
 	other.m_width = 0;
 	other.m_height = 0;
 	other.m_channels = 0;
+	other.m_hasMipmaps = false;
 }
 
 Texture::~Texture() {
@@ -99,11 +170,33 @@ Texture::~Texture() {
 	}
 }
 
-void Texture::updateData(int xOffset, int yOffset, unsigned int width, unsigned int height, const void* data) const {
+void Texture::createMipmaps() {
+	if (m_rendererId == 0)
+        throw std::runtime_error("Invalid state of Texture with id 0");
+	if (m_type == TextureType::Depth)
+		throw std::runtime_error("Mipmaps are not supported for depth textures");
+
+	if(!m_hasMipmaps) {
+		GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+		GLCALL(glGenerateMipmap(GL_TEXTURE_2D));
+
+		// Update minification filter so that mipmaps take effect
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, toOpenGLMinFilterModeWithMipmap(m_baseFilter, m_mipmapFilter)));
+		m_hasMipmaps = true;
+	}
+}
+
+void Texture::updateData(int xOffset, int yOffset, unsigned int width, unsigned int height, const void *data) const {
+    if (m_rendererId == 0)
+        throw std::runtime_error("Invalid state of Texture with id 0");
+	if (m_type == TextureType::Depth)
+		throw std::runtime_error("Depth textures should ideally not be updated, if still needed then do so manually and unbind it from active frame buffers");
+	if (width > m_width || height > m_height)
+		throw std::runtime_error("Can not update Texture since update size is greater than texture itself");
+
 	GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
-	TextureFormat format = getTextureFormat(m_type, m_channels);
+	TextureFormat format = toOpenGLTexFormat(m_type, m_channels);
 	GLCALL(glTexSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, width, height, format.inputFormat, format.type, data));
-	GLCALL(glBindTexture(GL_TEXTURE_2D, 0));
 }
 
 void Texture::bind(unsigned int slot) const {
@@ -112,6 +205,45 @@ void Texture::bind(unsigned int slot) const {
 
 	GLCALL(glActiveTexture(GL_TEXTURE0 + slot));
 	GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+}
+
+void Texture::setTextureFilterMode(TextureFilter filterMode) {
+	if (m_rendererId == 0)
+        throw std::runtime_error("Invalid state of Texture with id 0");
+
+	if (m_baseFilter != filterMode) {
+		m_baseFilter = filterMode;
+		GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+		// Update min filter taking mipmaps into account if they are generated
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_hasMipmaps ? toOpenGLMinFilterModeWithMipmap(m_baseFilter, m_mipmapFilter) : toOpenGLFilterMode(m_baseFilter)));
+		// Update mag filter (always just nearest or linear - mipmaps have no effect here)
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, toOpenGLFilterMode(m_baseFilter)));
+	}
+}
+
+void Texture::setMipmapFilterMode(TextureFilter filterMode) {
+	if (m_rendererId == 0)
+		throw std::runtime_error("Invalid state of Texture with id 0");
+
+	bool changed = m_mipmapFilter != filterMode;
+	m_mipmapFilter = filterMode; // Still store change in the case of createMipmaps() beeing called after this setter instead of before
+	if (m_hasMipmaps && changed) {
+		GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, toOpenGLMinFilterModeWithMipmap(m_baseFilter, m_mipmapFilter))); // Mip mapping
+	}
+}
+
+void Texture::setTextureWrapMode(TextureWrap wrapMode) {
+	if (m_rendererId == 0)
+        throw std::runtime_error("Invalid state of Texture with id 0");
+
+	if (m_wrapMode != wrapMode) {
+		m_wrapMode = wrapMode;
+		GLenum wrapParam = toOpenGLWrapMode(m_wrapMode);
+		GLCALL(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapParam));
+		GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapParam));
+	}
 }
 
 Texture& Texture::operator=(Texture&& other) noexcept {
@@ -129,10 +261,15 @@ Texture& Texture::operator=(Texture&& other) noexcept {
 		m_width = other.m_width;
 		m_height = other.m_height;
 		m_channels = other.m_channels;
+		m_baseFilter = other.m_baseFilter;
+		m_mipmapFilter =other.m_mipmapFilter;
+		m_wrapMode = other.m_wrapMode;
+		m_hasMipmaps = other.m_hasMipmaps;
 
 		other.m_width = 0;
 		other.m_height = 0;
 		other.m_channels = 0;
+		other.m_hasMipmaps = false;
 	}
 	return *this;
 }
