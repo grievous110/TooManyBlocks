59b6276ea03f427b2c1433083588db9513bd7fe4
GrivianDev <grivian-dev@posteo.de>
2025-03-06 01:09:56 +0100

    Attaching human readable opengl error enum

--- src/core/engine/rendering/GLUtils.cpp
+++ src/core/engine/rendering/GLUtils.cpp
@@ -5,6 +5,19 @@
 
 static constexpr int maxErrorChecks = 10;
 
+static constexpr const char* toGLErrorString(GLenum error) {
+	switch (error) {
+		case GL_INVALID_ENUM: return "GL_INVALID_ENUM";
+		case GL_INVALID_VALUE: return "GL_INVALID_VALUE";
+		case GL_INVALID_OPERATION: return "GL_INVALID_OPERATION";
+		case GL_STACK_OVERFLOW: return "GL_STACK_OVERFLOW";
+		case GL_STACK_UNDERFLOW: return "GL_STACK_UNDERFLOW";
+		case GL_OUT_OF_MEMORY: return "GL_OUT_OF_MEMORY";
+		case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FRAMEBUFFER_OPERATION";
+		default: return "Unknown OpenGL Error";
+	}
+}
+
 void GLClearError() {
     int errorCount = 0;
     while (glGetError() != GL_NO_ERROR) {
@@ -19,8 +32,8 @@ bool GLLogCall(const char* functionName, const char* file, int line) {
     int errorCount = 0;
 	GLenum error;
 	while ((error = glGetError()) != GL_NO_ERROR) {
-		std::stringstream stream;
-		stream << "[OpenGL Error 0x" << std::hex << error <<
+		std::ostringstream stream;
+		stream << "[OpenGL Error " << toGLErrorString(error) << " 0x" << std::hex << error <<
 			"] caused by " << functionName <<
 			" in " << file <<
 			" line " << line;
