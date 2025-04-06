78efaa73150f539ea38a6845f95476d13885e2fc
GrivianDev <grivian-dev@posteo.de>
2025-04-06 21:55:44 +0200

    More types and biome declarations

--- src/core/datatypes/BlockTypes.h
+++ src/core/datatypes/BlockTypes.h
@@ -1,8 +1,21 @@
 #ifndef BLOCKTYPES_H
 #define BLOCKTYPES_H
 
-#define STONE 0U
-#define GRASS 1U
-#define DIRT 2U
+#define AIR 0U
+#define STONE 1U
+#define BLACKSTONE 2U
+#define BLACKSTONE_FOSSIL 3U
+#define DIRT 4U
+#define GRASS 5U
+#define SAND 6U
+#define IRON_ORE 7U
+#define COAL_ORE 8U
+#define COPPER_ORE 9U
+#define WATER 10U
+#define MOSS 11U
+#define MUD 12U
+#define TITANIUM_ORE 13U
+#define PYRITE_ORE 14U
+#define OPAL_ORE 15U
 
 #endif
