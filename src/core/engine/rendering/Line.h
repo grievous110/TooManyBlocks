6e0ebda0cc54ea70edff7a63ac174b8609206fd7
GrivianDev <grivian-dev@posteo.de>
2025-04-06 23:07:13 +0200

    Line width control + adjustments

--- src/core/engine/rendering/Line.h
+++ src/core/engine/rendering/Line.h
@@ -13,12 +13,17 @@ private:
     std::unique_ptr<VertexBuffer> m_vbo;
     std::shared_ptr<Material> m_material;
     BoundingBox m_bounds;
+    float m_lineWidth;
 
     void draw() const override;
 
 public:
-    Line(const glm::vec3& start, const glm::vec3& end, std::shared_ptr<Material> material = nullptr);
+    Line(const glm::vec3& start, const glm::vec3& end, float lineWidth = 2.0f, std::shared_ptr<Material> material = nullptr);
     virtual ~Line() = default;
+
+    inline void setLineWidth(float lineWidth) { m_lineWidth = lineWidth; }
+
+    inline float getLineWidht() const { return m_lineWidth; }
 
     inline void assignMaterial(std::shared_ptr<Material> material) { m_material = material; }
 
