cb8374ef34662ae43916ae0e4f94fdaa99d4dbe2
GrivianDev <grivian-dev@posteo.de>
2025-04-08 22:44:01 +0200

    Added movement mode declaration

--- src/core/engine/comp/MovementComponent.h
+++ src/core/engine/comp/MovementComponent.h
@@ -8,9 +8,17 @@
 
 class Entity;
 
+enum class MovementMode {
+    Walk,
+    Fly,
+    Swim,
+	Spectator
+}; 
+
 class MovementComponent : public Updatable {
 private:
 	Entity* owner;
+	MovementMode m_mmode;
 	glm::vec3 m_velocity;
 	bool m_gravityEnabled;
 
@@ -18,8 +26,11 @@ public:
 	MovementComponent(Entity* owner);
 	virtual ~MovementComponent() = default;
 
+	inline MovementMode getMovementMode() const { return m_mmode; }
+	inline void setMovementMode(MovementMode mode) { m_mmode = mode; }
 	inline glm::vec3 getVelocity() const { return m_velocity; }
 	inline void setVelocity(const glm::vec3& velocity) { m_velocity = velocity; }
+	inline bool isGravityEnabled() const { return m_gravityEnabled; }
 	inline void setGravityEnabled(bool enabled) { m_gravityEnabled = enabled; }
 	
 	void update(float msDelta) override;
