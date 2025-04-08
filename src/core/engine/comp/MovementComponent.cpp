bb712f8919bb6d1ed7efaa6f568922dd48112c9a
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-04-08 22:44:01 +0200

    Added movement mode declaration

--- src/core/engine/comp/MovementComponent.cpp
+++ src/core/engine/comp/MovementComponent.cpp
@@ -1,7 +1,7 @@
 #include "engine/entity/Entity.h"
 #include "MovementComponent.h"
 
-MovementComponent::MovementComponent(Entity* owner) : owner(owner), m_velocity(glm::vec3(0.0f)), m_gravityEnabled(false) {}
+MovementComponent::MovementComponent(Entity* owner) : owner(owner), m_mmode(MovementMode::Spectator), m_velocity(0.0f), m_gravityEnabled(false) {}
 
 void MovementComponent::update(float msDelta) {
 	if (m_gravityEnabled) {
