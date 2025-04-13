6e1dcc1422bbc3f63f1013fd1702131487866225
GrivianDev <grivian-dev@posteo.de>
2025-04-13 14:23:29 +0200

    Logic for siwtching movement modes

--- src/core/engine/controllers/PlayerController.cpp
+++ src/core/engine/controllers/PlayerController.cpp
@@ -24,7 +24,7 @@ void PlayerController::notify(KeyEvent event, KeyEventData data) {
 }
 
 void PlayerController::notify(MousEvent event, MouseEventData data) {
-    if (event == MousEvent::Move && !keyStates[GLFW_KEY_LEFT_ALT]) {
+    if (!keyStates[GLFW_KEY_LEFT_ALT] && event == MousEvent::Move) {
 		if(Player* pl = dynamic_cast<Player*>(m_possessedEntity)) {
             Transform& tr = pl->getCamera()->getLocalTransform();
 
@@ -39,18 +39,20 @@ void PlayerController::notify(MousEvent event, MouseEventData data) {
             tr.rotate(allowedPitchDelta, tr.getRight());
         }
 	} else if (event == MousEvent::ButtonDown || event == MousEvent::ButtonUp) {
-        if (!keyStates[GLFW_MOUSE_BUTTON_LEFT] && data.key.code == GLFW_MOUSE_BUTTON_LEFT && event == MousEvent::ButtonDown) {
-            // Block breaking logic
-            if (ApplicationContext* context = Application::getContext()) {
+        if (!keyStates[GLFW_KEY_LEFT_ALT]) {
+            if (!keyStates[GLFW_MOUSE_BUTTON_LEFT] && data.key.code == GLFW_MOUSE_BUTTON_LEFT && event == MousEvent::ButtonDown) {
+                // Block breaking logic
+                if (ApplicationContext* context = Application::getContext()) {
+                    if(Player* pl = dynamic_cast<Player*>(m_possessedEntity)) {
+                        context->instance->m_world->setBlock(pl->getFocusedBlock(), AIR);
+                    }
+                }            
+            }
+            if (!keyStates[GLFW_MOUSE_BUTTON_RIGHT] && data.key.code == GLFW_MOUSE_BUTTON_RIGHT && event == MousEvent::ButtonDown) {
+                // Teleport player up (to unstuck)
                 if(Player* pl = dynamic_cast<Player*>(m_possessedEntity)) {
-                    context->instance->m_world->setBlock(pl->getFocusedBlock(), AIR);
+                    pl->getTransform().setPosition(pl->getTransform().getPosition() + glm::vec3(0, 5, 0));
                 }
-            }            
-        }
-        if (!keyStates[GLFW_MOUSE_BUTTON_RIGHT] && data.key.code == GLFW_MOUSE_BUTTON_RIGHT && event == MousEvent::ButtonDown) {
-            // Teleport player up (to unstuck)
-            if(Player* pl = dynamic_cast<Player*>(m_possessedEntity)) {
-                pl->getTransform().setPosition(pl->getTransform().getPosition() + glm::vec3(0, 5, 0));
             }
         }
 
@@ -83,44 +85,46 @@ void PlayerController::update(float deltaTime) {
                 }
             } else {
                 // Movement logic
-                glm::vec3 dir(0.0f);
-                const Transform camGlobTransform = pl->getCamera()->getGlobalTransform();
-                const Transform& camTransform = pl->getCamera()->getLocalTransform();
-                float speed = 4.0f;
-                MovementMode currMode = pl->getMovementComponent()->getMovementMode();
-                if (keyStates[GLFW_KEY_LEFT_SHIFT] && currMode != MovementMode::Fly) {
-                    speed *= 2.0f;
-                }
-        
-                // Adjust velocity based on key states
-                if (keyStates[GLFW_KEY_W]) dir += camTransform.getForward();
-                if (keyStates[GLFW_KEY_S]) dir -= camTransform.getForward();
-                if (keyStates[GLFW_KEY_D]) dir += camTransform.getRight();
-                if (keyStates[GLFW_KEY_A]) dir -= camTransform.getRight();
-        
-        
-                if (currMode == MovementMode::Spectator) {
-                    // Additional controls as spectator
-                    if (keyStates[GLFW_KEY_SPACE]) dir += WorldUp;
-                    if (keyStates[GLFW_KEY_LEFT_CONTROL]) dir -= WorldUp;
-                } else if (currMode == MovementMode::Walk) {
-                    // Jump logic
-                    if (keyStates[GLFW_KEY_SPACE] && pl->getMovementComponent()->isGrounded()) {
-                        pl->getMovementComponent()->addImpulse(WorldUp * 7.0f);
+                if (!keyStates[GLFW_KEY_LEFT_ALT]) {
+                    glm::vec3 dir(0.0f);
+                    const Transform camGlobTransform = pl->getCamera()->getGlobalTransform();
+                    const Transform& camTransform = pl->getCamera()->getLocalTransform();
+                    float speed = 4.0f;
+                    MovementMode currMode = pl->getMovementComponent()->getMovementMode();
+                    if (keyStates[GLFW_KEY_LEFT_SHIFT] && currMode != MovementMode::Fly) {
+                        speed *= 2.0f;
                     }
-                }
-                if (glm::length(dir) <= 0.0f) {
-                    speed = 0.0f;
-                }
-        
-                pl->getMovementComponent()->setInputDirection(dir);
-                pl->getMovementComponent()->setMovementSpeed(speed);
-        
-                // Block focus logic
-                HitResult result = linetraceByChannel(camGlobTransform.getPosition(), camGlobTransform.getPosition() + (camGlobTransform.getForward() * pl->getReachDistance()), Channel::BlockTrace);
-                pl->setIsFocusingBlock(result.hitSuccess);
-                if (result.hitSuccess) {
-                    pl->setFocusedBlock(glm::ivec3(result.objectPosition));
+            
+                    // Adjust velocity based on key states
+                    if (keyStates[GLFW_KEY_W]) dir += camTransform.getForward();
+                    if (keyStates[GLFW_KEY_S]) dir -= camTransform.getForward();
+                    if (keyStates[GLFW_KEY_D]) dir += camTransform.getRight();
+                    if (keyStates[GLFW_KEY_A]) dir -= camTransform.getRight();
+            
+            
+                    if (currMode == MovementMode::Spectator) {
+                        // Additional controls as spectator
+                        if (keyStates[GLFW_KEY_SPACE]) dir += WorldUp;
+                        if (keyStates[GLFW_KEY_LEFT_CONTROL]) dir -= WorldUp;
+                    } else if (currMode == MovementMode::Walk) {
+                        // Jump logic
+                        if (keyStates[GLFW_KEY_SPACE] && pl->getMovementComponent()->isGrounded()) {
+                            pl->getMovementComponent()->addImpulse(WorldUp * 7.0f);
+                        }
+                    }
+                    if (glm::length(dir) <= 0.0f) {
+                        speed = 0.0f;
+                    }
+            
+                    pl->getMovementComponent()->setInputDirection(dir);
+                    pl->getMovementComponent()->setMovementSpeed(speed);
+            
+                    // Block focus logic
+                    HitResult result = linetraceByChannel(camGlobTransform.getPosition(), camGlobTransform.getPosition() + (camGlobTransform.getForward() * pl->getReachDistance()), Channel::BlockTrace);
+                    pl->setIsFocusingBlock(result.hitSuccess);
+                    if (result.hitSuccess) {
+                        pl->setFocusedBlock(glm::ivec3(result.objectPosition));
+                    }
                 }
             }
         }
