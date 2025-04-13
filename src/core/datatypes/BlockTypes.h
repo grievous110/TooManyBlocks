3ce0061cf6baa6fb815e885cfcc00025a2387bfa
GrivianDev <grivian-dev@posteo.de>
2025-04-13 17:49:39 +0200

    More blocks

--- src/core/datatypes/BlockTypes.h
+++ src/core/datatypes/BlockTypes.h
@@ -12,10 +12,31 @@
 #define COAL_ORE 8U
 #define COPPER_ORE 9U
 #define WATER 10U
-#define MOSS 11U
+#define GROUNDWEAVE 11U
 #define MUD 12U
 #define TITANIUM_ORE 13U
 #define PYRITE_ORE 14U
 #define OPAL_ORE 15U
+#define AMETHYST_ORE 16U
+#define AMETHYST 17U
+#define OAK_WOOD 18U
+#define OAK_LEAVES 19U
+#define OAK_PLANKS 20U
+#define CRACKED_VAULT_BLOCK 21U
+#define VAULT_BLOCK 22U
+#define SEALED_GOLD 23U
+#define OBSIDIUM 24U
+#define ANDESITE 25U
+#define VERDITE_ORE 26U
+#define AMBER_RESIN 27U
+#define CHLORITE_ORE 28U
+#define MOSS 29U
+#define MOSSY_ANDESITE 30U
+#define GRANITE 31U
+#define RUNED_SILVER_ORE 32U
+#define AETHERSTONE_ORE 33U
+#define PHANTOM_ORE 34U
+#define GRANITE_BRICK 35U
+
 
 #endif
