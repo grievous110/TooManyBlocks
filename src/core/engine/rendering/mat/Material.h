e9573a1a68cf88f564c035479d1fde485d9b6dd6
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-06-15 13:41:48 +0200

    transparent flag for renderables

--- src/core/engine/rendering/mat/Material.h
+++ src/core/engine/rendering/mat/Material.h
@@ -14,6 +14,8 @@ class Material {
 public:
     virtual ~Material() = default;
 
+    virtual bool isTransparent() const { return false; }
+
     virtual bool supportsPass(PassType passType) const = 0;
     virtual void bindForPass(PassType passType, const RenderContext& context) const = 0;
     virtual void bindForObjectDraw(PassType passType, const RenderContext& context) const = 0;
