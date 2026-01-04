21a74c269b73df79c4aadc93f8d581f6259e6440
Joel <grievous@posteo.de>
2026-01-04 14:33:15 +0100

    toGPULight() function on lights

--- src/core/engine/env/lights/Spotlight.cpp
+++ src/core/engine/env/lights/Spotlight.cpp
@@ -1,8 +1,16 @@
 #include "Spotlight.h"
 
+Spotlight::Spotlight(const glm::vec3& color, float intensity, float fovy, float range)
+    : Light(LightType::Spot, color, intensity, range) {
+    m_internal.fovy = fovy;
+    m_internal.innerCutoffAngle = fovy;
+}
+
 LightType Spotlight::getType() const { return LightType::Spot; }
 
-glm::mat4 Spotlight::getProjectionMatrix() const { return glm::perspective(glm::radians(m_fovy), 1.0f, 0.1f, m_range); }
+glm::mat4 Spotlight::getProjectionMatrix() const {
+    return glm::perspective(glm::radians(m_internal.fovy), 1.0f, 0.1f, m_internal.range);
+}
 
 glm::mat4 Spotlight::getViewMatrix() const {
     Transform tr = getGlobalTransform();
